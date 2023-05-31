8daf93e7a88086cd2c1d8cc5a7a04ac6c3660126
bhupverz <bhupesh.verz@gmail.com>
2023-05-31 11:46:03 +0530

    changes

--- resources/views/inc/headerstudent.blade.php
+++ resources/views/inc/headerstudent.blade.php
@@ -1,12 +1,12 @@
 <header class="header-container">
     <div class="logo">
-        <a href="index.html">
+        <a href="{{ url('home') }}">
             <img src="images/3g-abacus-logo.png" alt="3G Abacus" />
         </a>
     </div>
     <div class="actions">
         <a class="cart" href="checkout-step-1.html"><i class="icon-cart"></i><strong>+99</strong></a>
-        <div class="headuser hide-991"><span class="welcome">Welcome back Michelle,</span>
+        <div class="headuser hide-991"><span class="welcome">Welcome back {{ Auth::user()->name }},</span>
             <a href="{{url('logout')}}">Logout</a>
         </div>
         <div class="dropdown show-991">
@@ -14,8 +14,8 @@
             <ul class="dropdown-menu">
                 <li class="active"><a href="be-overview.html">Overview</a></li>
                 <li><a href="be-achievements.html">My Achievements</a></li>
-                <li><a href="be-profile.html">My Profile</a></li>
-                <li><a href="index.html">Logout</a></li>
+                <li><a href="{{url('my-profile')}}">My Profile</a></li>
+                <li><a href="{{url('logout')}}">Logout</a></li>
             </ul>
         </div>
     </div>
