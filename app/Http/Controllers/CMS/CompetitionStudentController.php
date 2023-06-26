8f63c9489cbeb10342348a8d5fc8c3a3a39d3693
bhupverz <bhupesh.verz@gmail.com>
2023-06-26 09:13:35 +0530

    changes

---
+++ app/Http/Controllers/CMS/CompetitionStudentController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\CMS;
+
+use App\CompetitionStudent;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Traits\SystemSettingTrait;
+
+class CompetitionStudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CompetitionStudent  $competitionStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CompetitionStudent $competitionStudent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CompetitionStudent  $competitionStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CompetitionStudent $competitionStudent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CompetitionStudent  $competitionStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CompetitionStudent $competitionStudent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CompetitionStudent  $competitionStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CompetitionStudent $competitionStudent)
+    {
+        //
+    }
+}
