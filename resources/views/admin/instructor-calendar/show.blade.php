30d8ea6a306748e9c5e68bc2f0f319a8928f4f85
jafirverz <40601286+jafirverz@users.noreply.github.com>
2023-07-17 17:38:10 +0530

    calendar

--- resources/views/admin/instructor-calendar/show.blade.php
+++ resources/views/admin/instructor-calendar/show.blade.php
@@ -6,11 +6,11 @@
     <section class="section">
         <div class="section-header">
             <div class="section-header-back">
-                <a href="{{ route('teaching-materials.index') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+                <a href="{{ route('instructor-calendar.index') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
             </div>
             <h1>{{ $title ?? '-' }}</h1>
-            @include('admin.inc.breadcrumb', ['breadcrumbs' => Breadcrumbs::generate('admin_teaching_materials_crud', 'Show',
-            route('teaching-materials.show', $material->id))])
+            @include('admin.inc.breadcrumb', ['breadcrumbs' => Breadcrumbs::generate('admin_instructor_calendar_crud', 'Show',
+            route('instructor-calendar.show', $calendar->id))])
         </div>
 
         <div class="section-body">
@@ -21,25 +21,20 @@
                         <div class="card-body">
                             <div class="card-body">
                                 <div class="form-group">
-                                    <strong>Title</strong>: {{ $material->title }}
+                                    <strong>Full Name</strong>: {{ $calendar->full_name ?? '' }}
+                                </div>
+
+                                <div class="form-group">
+                                    <strong>Note</strong>: {{ $calendar->note ?? '' }}
                                 </div>
                                 <div class="form-group">
-                                    <strong>Uploaded Files</strong>:  <a href="{{ asset($material->uploaded_files) }}" target="_blank">{{ asset($material->uploaded_files) }}</a>
+                                    <strong>Teacher</strong>: {{ $calendar->teacher->name }}
                                 </div>
                                 <div class="form-group">
-                                    <strong>Title</strong>: {{ $material->title }}
+                                    <strong>Created At</strong>: {{ $calendar->created_at->format('d M, Y h:i A') }}
                                 </div>
                                 <div class="form-group">
-                                    <strong>Description</strong>: {{ $material->description }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>Teacher</strong>: {{ $material->teacher->name }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>Created At</strong>: {{ $material->created_at->format('d M, Y h:i A') }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>Updated At</strong>: {{ $material->updated_at->format('d M, Y h:i A') }}
+                                    <strong>Updated At</strong>: {{ $calendar->updated_at->format('d M, Y h:i A') }}
                                 </div>
                             </div>
                         </div>
