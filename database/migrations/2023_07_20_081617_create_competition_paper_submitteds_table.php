56b16f415a9dccf56467c02537370e52aeff3ebd
bhupverz <bhupesh.verz@gmail.com>
2023-07-21 11:21:14 +0530

    changes

--- database/migrations/2023_07_20_081617_create_competition_paper_submitteds_table.php
+++ database/migrations/2023_07_20_081617_create_competition_paper_submitteds_table.php
@@ -19,6 +19,8 @@ class CreateCompetitionPaperSubmittedsTable extends Migration
             $table->foreign('competition_id')->references('id')->on('competition_controllers');
             $table->unsignedBigInteger('competition_paper_id')->nullable();
             $table->foreign('competition_paper_id')->references('id')->on('competition_papers');
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('competition_categories');
             $table->unsignedBigInteger('user_id')->nullable();
             $table->string('paper_type')->nullable();
             $table->foreign('user_id')->references('id')->on('users');
