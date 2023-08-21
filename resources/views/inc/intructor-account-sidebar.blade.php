bfbdb8333ff30e66922bd0e69be7e5be2220a78e
bhupverz <bhupesh.verz@gmail.com>
2023-08-21 11:20:29 +0530

    changes

--- resources/views/inc/intructor-account-sidebar.blade.php
+++ resources/views/inc/intructor-account-sidebar.blade.php
@@ -1,24 +1,24 @@
 <h3>My Dashboard</h3>
 <ul>
-    <li>
+    <li @if(Request::segment(1) == '' || Request::segment(1) == 'home') class="active" @endif>
         <a href="{{ url('/') }}">
             <span><img src="{{ asset('images/tempt/ico-overview.png') }}" alt="Overview icon" /></span>
             <strong>Overview</strong>
         </a>
     </li>
-    <li class="active">
+    <li @if(Request::segment(1) == 'instructor-profile') class="active" @endif>
         <a href="{{ url('instructor-profile') }}">
             <span><img src="{{ asset('images/tempt/ico-profile.png') }}" alt="Profile icon" /></span>
             <strong>My Profile</strong>
         </a>
     </li>
-    <li>
+    <li @if(Request::segment(1) == 'instructor-students') class="active" @endif>
         <a href="{{ url('/instructor-students') }}">
             <span><img src="{{ asset('images/tempt/ico-students.png') }}" alt="Students icon" /></span>
             <strong>My Students</strong>
         </a>
     </li>
-    <li>
+    <li @if(Request::segment(1) == 'teaching-materials') class="active" @endif>
 
         <a href="{{ url('teaching-materials') }}">
 
@@ -26,19 +26,19 @@
             <strong>Teaching Materials</strong>
         </a>
     </li>
-    <li>
+    <li @if(Request::segment(1) == 'grading-examination') class="active" @endif>
         <a href="{{ url('grading-examination') }}">
             <span><img src="{{ asset('images/tempt/ico-grading.png') }}" alt="Grading icon" /></span>
             <strong>Grading Examinations</strong>
         </a>
     </li>
-    <li>
+    <li @if(Request::segment(1) == 'instructor-competition') class="active" @endif>
         <a href="{{ url('instructor-competition') }}">
             <span><img src="{{ asset('images/tempt/ico-competitions.png') }}" alt="Competitions icon" /></span>
             <strong>Competitions</strong>
         </a>
     </li>
-    <li>
+    <li @if(Request::segment(1) == 'allocation') class="active" @endif>
         <a href="{{ url('allocation') }}">
             <span><img src="{{ asset('images/tempt/ico-allocation.png') }}" alt="Allocation icon" /></span>
             <strong>Test and Survey Allocation</strong>
