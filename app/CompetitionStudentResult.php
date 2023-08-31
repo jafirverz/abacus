645128dfee7081458aabdcdaee1f46ef15ed9b3c
bhupverz <bhupesh.verz@gmail.com>
2023-08-31 10:20:46 +0530

    changes

--- app/CompetitionStudentResult.php
+++ app/CompetitionStudentResult.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class CompetitionStudentResult extends Model
 {
     //
+    public function competition(){
+        return $this->belongsTo('App\Competition', 'competition_id', 'id');
+    }
+
+    public function category(){
+        return $this->belongsTo('App\CompetitionCategory', 'category_id', 'id');
+    }
 }
--- app/GradingStudentResults.php
+++ app/GradingStudentResults.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class GradingStudentResults extends Model
 {
     //
+    public function grading(){
+        return $this->belongsTo('App\GradingExam', 'grading_id', 'id');
+    }
+
+    public function category(){
+        return $this->belongsTo('App\CompetitionCategory', 'category_id', 'id');
+    }
 }
