37dee359449166f2aad91a9c6610851682d50c22
bhupverz <bhupesh.verz@gmail.com>
2023-11-28 14:25:12 +0530

    changes

--- app/Http/Controllers/CMS/OrderController.php
+++ app/Http/Controllers/CMS/OrderController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\CMS;
 
+use App\Country;
 use App\Http\Controllers\Controller;
+use App\Level;
 use App\Order;
 use App\OrderDetail;
 use Illuminate\Http\Request;
 use App\Traits\SystemSettingTrait;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
@@ -38,5 +41,46 @@ class OrderController extends Controller
         $title = $this->title;
         $orderDetails = OrderDetail::where('order_id', $id)->get();
         return view('admin.cms.orders.show', compact('title', 'orderDetails'));
+    }
+
+    public function create()
+    {
+        $title = $this->title;
+        $students = User::whereIn('user_type_id', [1,2])->orderBy('id', 'desc')->get();
+        $levels = Level::get();
+        return view('admin.cms.orders.create', compact('title', 'students', 'levels'));
+    }
+
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $request->validate([
+            'amount'  =>  'required',
+            'studen'    =>  'required',
+            'expirydate'   =>  'required',
+            'level'   =>  'required',
+        ]);
+        $user = User::where('id', $request->studen)->first();
+        $order = new Order();
+        $order->user_id = $request->studen;
+        $order->country_id = $user->country_code;
+        $order->total_amount = $request->amount;
+        $order->payment_status = 'COMPLETED';
+        $order->save();
+
+        foreach($request->level as $levee){
+            $level = Level::where('id', $levee)->first();
+            $orderDetails = new OrderDetail();
+            $orderDetails->user_id = $request->studen;
+            $orderDetails->order_id = $order->id;
+            $orderDetails->level_id = $levee;
+            $orderDetails->order_type = 'level';
+            $orderDetails->name = $level->title;
+            $orderDetails->amount = $request->amount;
+            $orderDetails->expiry_date = $request->expirydate;
+            $orderDetails->save();
+        }
+        return redirect()->route('orders.index')->with('success', __('constant.CREATED', ['module' => $this->title]));
+        //return view('admin.cms.orders.create', compact('title', 'students', 'levels'));
     }
 }
