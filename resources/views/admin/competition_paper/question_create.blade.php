6e70628ec9a0778f9f75a5ae0ec0e0f3926391b0
bhupverz <bhupesh.verz@gmail.com>
2023-11-30 10:16:42 +0530

    changes

--- resources/views/admin/competition_paper/question_create.blade.php
+++ resources/views/admin/competition_paper/question_create.blade.php
@@ -6,7 +6,7 @@
     <section class="section">
         <div class="section-header">
             <div class="section-header-back">
-                <a href="{{ route('comp-questions.index') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+                <a href="{{ route('papers.questions', ['id' => $pId]) }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
             </div>
             <h1>{{ $title ?? '-' }}</h1>
 {{--            @include('admin.inc.breadcrumb', ['breadcrumbs' => Breadcrumbs::generate('admin_bank_crud', 'Create', route('bank.create'))])--}}
