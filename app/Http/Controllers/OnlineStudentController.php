65666b08c21ede50746115068123402b840e4bca
bhupverz <bhupesh.verz@gmail.com>
2023-12-12 14:34:44 +0530

    changes

--- app/Http/Controllers/OnlineStudentController.php
+++ app/Http/Controllers/OnlineStudentController.php
@@ -196,10 +196,15 @@ class OnlineStudentController extends Controller
     }
 
     public function downloadCertificate( $id = null){
+
         $courseSubmitted = CourseSubmitted::where('id', $id)->first();
         $certificate = Certificate::where('id', $courseSubmitted->certificate_id)->first();
-        $key = ['{{course_name}}','{{full_name}}','{{email}}','{{dob}}','{{contact_number}}','{{address}}'];
-        $value = [$courseSubmitted->course->title, Auth::user()->name, Auth::user()->email, Auth::user()->dob, Auth::user()->mobile, Auth::user()->address];
+        $logo='<img style="width: 300px" width="300" src="http://abacus.verz1.com/storage/site_logo/20230522101759_3g-abacus-logo.png" alt="abacus" />';
+        $logoFoot='<img style="width: 300px" width="300" src="http://abacus.verz1.com/storage/images/1702371736__65782198b8449__3g-abacus-foot.png" alt="abacus" />';
+        $bg = 'http://abacus.verz1.com/storage/images/1702371744__657821a0f0a96__bg-certificate-2.jpg';
+
+        $key = ['{{course_name}}','{{level_name}}','{{user_name}}','{{date_of_issue_certificate}}','{{logo}}','{{logofoot}}','{{bg}}'];
+        $value = [$courseSubmitted->course->title,'Preparatory Level', Auth::user()->name, 'NA', $logo, $bg];
         $newContents = str_replace($key, $value, $certificate->content);
         $pdf = PDF::loadView('account.certificate_pdf', compact('newContents'));
         return $pdf->download('certificate.pdf');
