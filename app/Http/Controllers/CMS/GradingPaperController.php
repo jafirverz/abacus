0f8c2537e4384c4a65128760d3c9d9175f165c55
bhupverz <bhupesh.verz@gmail.com>
2024-06-10 09:44:20 +0530

    changes

--- app/Http/Controllers/CMS/GradingPaperController.php
+++ app/Http/Controllers/CMS/GradingPaperController.php
@@ -163,8 +163,11 @@ class GradingPaperController extends Controller
     public function update(Request $request, $id)
     {
         $messages = [
-            'questiontemplate.required_if' => 'This field is required',
             'paper_type.required_if' => 'This field is required',
+            'price.required_if' => 'Price field is required',
+            //'pdf_upload.required_if' => 'PDF Upload field is required',
+            'questiontemplate.required_if' => 'Question Template field is required',
+            'time.required_if' => 'Time field is required',
         ];
         $request->validate([
             'title'  =>  'required',
